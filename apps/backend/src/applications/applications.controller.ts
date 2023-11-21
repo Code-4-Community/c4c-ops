cbe0e2190819d1c3d6adf0173c4a57be9ce81b63
ananyar807 <ananyar807@gmail.com>
2023-11-20 19:22:20 -0500

    updated endpoint to use usersService

--- apps/backend/src/applications/applications.controller.ts
+++ apps/backend/src/applications/applications.controller.ts
@@ -12,23 +12,28 @@ import { CurrentUserInterceptor } from '../interceptors/current-user.interceptor
 import { AuthGuard } from '@nestjs/passport';
 import { ApplicationDTO } from './dto/application.dto';
 import { classToPlain, instanceToPlain, plainToClass } from 'class-transformer';
+import { UsersService } from '../users/users.service';
+import { getAppForCurrentCycle } from './utils';
 
 @Controller('apps')
 @UseInterceptors(CurrentUserInterceptor)
 @UseGuards(AuthGuard('jwt'))
 export class ApplicationsController {
-  constructor(private readonly applicationsService: ApplicationsService) {}
+  constructor(
+    private readonly applicationsService: ApplicationsService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Get('/:userId')
   async getApplication(
     @Param('userId', ParseIntPipe) userId: number,
+    //TODO: make req.user.applications unaccessible
     @Request() req,
   ): Promise<ApplicationDTO> {
-    const app = await this.applicationsService.findOne(req.user, userId);
+    const user = await this.usersService.findOne(req.user, userId);
+    const app = getAppForCurrentCycle(user.applications);
     const appObject = instanceToPlain(app);
-    // this is what i had orginally: appObject['numApps'] = this.applicationsService.getNumApps(req.user, userId)
-    //but does this make more sense:
-    appObject['numApps'] = app.application.length;
+    appObject['numApps'] = user.applications.length;
     return plainToClass(ApplicationDTO, appObject);
   }
 }
