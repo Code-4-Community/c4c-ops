3ae0851698d92fde31446c4d27b5ea8bab5e2956
Rayyan Mridha <mridha.r@northeastern.edu>
2025-08-20 11:21:15 -0400

    add table view based on current user

--- apps/frontend/src/containers/applications.tsx
+++ apps/frontend/src/containers/applications.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ApplicationTable } from '@components/ApplicationTables';
 import { RecruiterTable } from '@components/RecruiterView/Table';
+import { User } from '@components/types';
+import apiClient from '@api/apiClient';
+import useLoginContext from '@components/LoginPage/useLoginContext';
 
 const Applications: React.FC = () => {
-  return <RecruiterTable />;
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const { token: accessToken } = useLoginContext();
+
+  const getUser = async () => {
+    try {
+      const currentUser = await apiClient.getUser(accessToken);
+      setCurrentUser(currentUser);
+    } catch (error) {
+      console.error('Error fetching User:', error);
+    }
+  };
+
+  if (!currentUser) return <div>Loading...</div>;
+
+  return (
+    <>
+      {currentUser.status === 'Recruiter' ? (
+        <RecruiterTable />
+      ) : (
+        <ApplicationTable />
+      )}
+    </>
+  );
 };
 
 export default Applications;
