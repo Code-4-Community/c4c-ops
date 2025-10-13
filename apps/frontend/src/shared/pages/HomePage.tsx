920f070fdb5aee9bd8b1552e4cfec070bc716fc4
Naman Rusia <naman.rusia14@gmail.com>
2025-10-13 19:15:03 -0400

    added some notes

--- apps/frontend/src/shared/pages/HomePage.tsx
+++ apps/frontend/src/shared/pages/HomePage.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 // staring point for Naman and Tarun, Issue # 183
+/**
+ * Notes and questions:
+ * - under what authorization level does this file need to be in in @app.tsx?
+ * - can we download tailwindcss as a dependency (pretty please)
+ * - Is our task just to implement the one pager from figma, no backend needed?
+ * - is this file in the correct folder in the frontend (apps/frontend/src/shared/pages/HomePage.tsx)
+ */
 
 const HomePage = () => {
   return <div>index</div>;
