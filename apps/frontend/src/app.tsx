8dc04373b9de15bcb444a60a514600cc90eb8dfe
ryaken-nakamoto <ryakenNakamoto@gmail.com>
2025-10-30 18:04:42 -0400

    removed unused components + abstracted applications view

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -3,14 +3,11 @@ import { useState } from 'react';
 
 import ApplicantHomePage from '@features/applicant/pages/ApplicantHomePage';
 import NotFoundPage from '@shared/pages/NotFoundPage';
-import DashboardPage from '@shared/pages/DashboardPage';
 import ApplicationsPage from '@features/applications/pages/ApplicationsPage';
 import ApplicationDetailPage from '@features/applications/pages/ApplicationDetailPage';
-import SettingsPage from '@shared/pages/SettingsPage';
 import LoginContext from '@features/auth/components/LoginPage/LoginContext';
 import ProtectedRoutes from '@features/auth/components/ProtectedRoutes';
 import LoginPage from '@features/auth/components/LoginPage';
-import Navigation from '@shared/components/Navigation';
 import AdminRoutes from '@features/auth/components/AdminRoutes';
 import HomePage from '@shared/pages/HomePage';
 
@@ -25,37 +22,11 @@ export const App: React.FC = () => {
 
           <Route element={<ProtectedRoutes token={token} />}>
             <Route element={<AdminRoutes />}>
-              <Route
-                path="/"
-                element={
-                  <Navigation>
-                    <DashboardPage />
-                  </Navigation>
-                }
-              />
-              <Route
-                path="/applications"
-                element={
-                  <Navigation>
-                    <ApplicationsPage />
-                  </Navigation>
-                }
-              />
+              <Route path="/" element={<ApplicationsPage />} />
+              <Route path="/applications" element={<ApplicationsPage />} />
               <Route
                 path="/applications/:userId"
-                element={
-                  <Navigation>
-                    <ApplicationDetailPage />
-                  </Navigation>
-                }
-              />
-              <Route
-                path="/settings"
-                element={
-                  <Navigation>
-                    <SettingsPage />
-                  </Navigation>
-                }
+                element={<ApplicationDetailPage />}
               />
             </Route>
 
