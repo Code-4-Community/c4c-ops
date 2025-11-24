b66ab3b8afac4172d4b5841993a19528a2281227
Jayden Carvajal <147960616+jaydencarvajal511@users.noreply.github.com>
2025-11-24 17:06:21 -0500

    fixing the merge conflicts so that you can refresh on reload (#212)
    Co-authored-by: Jayden Carvajal <carvajal.ja@northeastern.edu>

--- apps/frontend/src/features/auth/components/ProtectedRoutes/index.tsx
+++ apps/frontend/src/features/auth/components/ProtectedRoutes/index.tsx
@@ -1,12 +1,14 @@
 import { Navigate, Outlet } from 'react-router-dom';
+import useLoginContext from '@features/auth/components/LoginPage/useLoginContext';
 
 /**
  * ProtectedRoutes renders the children components only
  * if the user is authenticated (i.e if an access token exists).
  * If the user is not authenticated, it redirects to the login page.
  */
-function ProtectedRoutes({ token }: { token: string }) {
-  return token ? <Outlet /> : <Navigate to="/login" />;
+function ProtectedRoutes() {
+  const { token } = useLoginContext();
+  return token ? <Outlet /> : <Navigate to="/home" replace />;
 }
 
 export default ProtectedRoutes;
